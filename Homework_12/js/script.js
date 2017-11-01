3e9a91d39dd52f272c0893de1d1d09dbdb5e1a6c
Aleksandra <PivovarAO@gmail.com>
2017-11-01 20:46:20 +0200

    uncomment

--- Homework_12/js/script.js
+++ Homework_12/js/script.js
@@ -1,17 +1,17 @@
 //Sum of numbers 1-100
-// var Sum = 0;
-// for (var i = 0; i <= 100; i++) {
-// 	Sum += i;
-// }
-// console.log("Sum =",Sum);
+var Sum = 0;
+for (var i = 0; i <= 100; i++) {
+	Sum += i;
+}
+console.log("Sum =",Sum);
 
 //factorial
-// var number = +(prompt("Enter a number"));
-// var factorial = 1;
-// for (var i = 1; i <= number-1; i++) {
-// 	factorial *= (i+1);
-// }
-// console.log("factorial for",number,"=" ,factorial);
+var number = +(prompt("Enter a number"));
+var factorial = 1;
+for (var i = 1; i <= number-1; i++) {
+	factorial *= (i+1);
+}
+console.log("factorial for",number,"=" ,factorial);
 
 //Hayes sequence
 var num = +(prompt("Enter a number"));
